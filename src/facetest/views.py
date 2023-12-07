3bcd37e99162516cbd85bb35fb39e199515cd75a
Grant Nieda <grantnieda@gmail.com>
2023-12-07 10:48:49 -0800

    commit before first azure cloud deployment

--- src/facetest/views.py
+++ src/facetest/views.py
@@ -18,7 +18,7 @@ app_secret = 'e0a58b1074b2237e90a152a1f4c156d0'
 long_term_user_access_token = 'EAAVacs9AvhEBO8FfsYkE9KFjZByjBqv36u5dQPjPemywZAypkqDr7I1edIx0E6kbBFimICWv9TEWyYrHcvFBmiQtHXPQsY1wU6XufviVV7A9uGxRCIVV7bW8nn1tEY8VA39KachRLvpBET3W3MlAFcdd5SDcZAVEtd8s4qy2NewZBevO9HurufS1'
 page_name = 'Extra Secret Page'
 #page_token = 'EAAVacs9AvhEBOzPD0VmeXihXwpEv0Be2YdZBR7XAqAfdUBrZC69Lwn567QlxBZC5E45OyEOkYPsz3it2d9JBNK1nZAYise5Hfi6I8AgNGp2A6zB5ZBaDbg9e7yvy91IT7a1H5FtPJc5BJw1aUZBsnKgyWmvGMPkFZBFYW8wpw5a4R8ZBYOFCG0DDZCepQOpbfbedus579rfdsCJ0ZAOXxadLPuxDUZD'
-page_token = None
+page_token = 'EAAVacs9AvhEBO4PZCHVCeN6srFaZB9D59WVzMYYZC8WzSvkyWcz3FnfF4ZCH6xIHZCNYjJqihd0X2LkTk07VniZB5DwC3oodRKP2ca9LY4FQraHLxB63xZB4ZCn9mb9JwFy3s6aSdBGxxaCTm4LUbQdtPu68haPMMNRoOTJuw1CV583K6UqMZAQRD1p76JTIyaaZBe63Vlgr4oOIYdURYWmDQmHLMZD'
 
 post_id = "2553705538124434_2552109628284025" # page#_post
 page_id = "155168831017376"
@@ -27,10 +27,9 @@ post_url = 'https://graph.facebook.com/{}/feed'.format(page_id)
 
 
 def facemsg_view(request):
-    refresh_page_token()
     recipient_id = "7254791501239920"
     url = f"https://graph.facebook.com/v18.0/{page_id}/messages"
-    message_id = "t_2565001643661490-oSnFgY4Gw"
+    #message_id = "t_2565001643661490-oSnFgY4Gw"
     if request.method == 'POST':
         msgform = FacebookMessageForm(request.POST)
         if msgform.is_valid():
@@ -42,6 +41,11 @@ def facemsg_view(request):
                 'message': {'text': msg},
                 'access_token': page_token,
             }
+            # json_test_data = {
+            #     'key1': 'v2',
+            #     'key2': 'v2',
+            # }
+            # return redirect('json_display_page', json_data = json_test_data)
 
             response = requests.post(url, json = data)
             if response.status_code == 200: # Successful response retrieval status code
@@ -58,6 +62,8 @@ def facemsg_view(request):
     }
     return render(request, 'facetest/facebook-msg.html', context)
 
+def json_display_view(request, json_data):
+    return JsonResponse(json_data)
 
 def facetest_view(request): #only handles post requests with facepost name
     if request.method == 'POST':
@@ -125,9 +131,11 @@ def display_my_facebook_post(request, *args, **kwargs): # Using chatGTP - USES p
         return HttpResponse("Failed to retrieve the post")
 
 def refresh_page_token():
+    #TODO: Only seems to work when copied and pasted in - not as a function
     user_graph_url = f'https://graph.facebook.com/v13.0/me/accounts?access_token={long_term_user_access_token}'
     response = requests.get(user_graph_url)
     data = response.json()
+    page_token = None
     page_id = None
     for page in data['data']:  # Parses json response
         if page['name'] == page_name:
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -32,6 +32,7 @@ from products.views import product_create_view_raw_django
 from facetest.views import display_my_facebook_post
 from facetest.views import facetest_view
 from facetest.views import facemsg_view
+from facetest.views import json_display_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -43,10 +44,10 @@ urlpatterns = [
     path('create-raw-html/', product_create_view_raw_html),
     path('create-raw-django/', product_create_view_raw_django),
 
-
     path('my-facebook-post/', display_my_facebook_post),
     path('facetest/', facetest_view),
     path('facemsg/', facemsg_view),
+    path('display_json/', json_display_view, name = 'json_display_page'),
 
 
 ]
