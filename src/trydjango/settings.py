7aae534a850736c72e96094c2f36ddae738dc9b4
Grant Nieda <grantnieda@gmail.com>
2023-12-07 11:23:00 -0800

    copy error

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -106,11 +106,13 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': parameters['dbname'],
-    'USER': parameters['user'],
-    'PASSWORD': parameters['password'],
-    'HOST': parameters['host'],
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters['dbname'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
+        'HOST': parameters['host'],
+    }
 
 }
 
