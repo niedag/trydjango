c7bbf784396e7d4973d71935f3d6c7f88e2cfb10
Grant Nieda <grantnieda@gmail.com>
2023-12-19 18:26:26 -0800

    migrated for fb social django changes

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -177,7 +177,7 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 # SMTP Server for sending password reset emails
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"  # Only good for local development - need to configure a
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
 # Additional backend settings for auth -- OAuth2 so django rest?
